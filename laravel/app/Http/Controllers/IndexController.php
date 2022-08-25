924ecfad33b2b07d6da36fd1ee260324b2093844
Khaled <khaled.el-moussa@aller.com>
2022-08-25 13:19:05 +0200

    Showing encrypting message

---
+++ laravel/app/Http/Controllers/IndexController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
+
+class IndexController extends Controller
+{
+    public function view() {
+        $word = "Flemming";
+        $encrypted_word = Crypt::encryptString($word);
+
+
+        return view('welcome', compact('word', 'encrypted_word'));
+    }
+}
